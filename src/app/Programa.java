afa9288a37aff5e3d058ef3b6f14d3fbecf9b9f5
MateusBarbosa <jmbp.ecp19@uea.edu.br>
2022-08-25 20:57:31 -0400

    commit 1

---
+++ src/app/Programa.java
@@ -0,0 +1,23 @@
+package app;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.conversor;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Qual o preço do dollar? ");
+		double dollarPrice = sc.nextDouble();
+		System.out.print("Quantos dólares serão comprados? ");
+		double amount = sc.nextDouble();
+		double result = conversor.dollarToReal(amount, dollarPrice);
+		System.out.printf("Valor a ser pago em reais = %.2f%n", result);
+		sc.close();
+		}
+
+}
